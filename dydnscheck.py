46d775ded08bc9282c14a6dd292e87e3e1b8afb7
user <user@jenkem.(none)>
2013-11-09 07:42:13 +0000

    Stopped polling the registrar every 5 mins.

--- dydnscheck.py
+++ dydnscheck.py
@@ -15,7 +15,7 @@ import sys, time, urllib, urllib2, datetime
 def fetch_external_ip():
     ''' get ip address '''
     try:
-        output  = unicode(urllib2.urlopen(u'http://ipecho.net/plain').read())
+        output  = urllib2.urlopen('http://ipecho.net/plain').read()
     except:
         output = None
     return output
@@ -46,30 +46,40 @@ def main():
     api_replace_t = 'REPLACE hame 3600 A {0}'
     api_list = 'LIST'
     time_out = 300 # seconds == 5 mins
+    dns_poll_interval = datetime.timedelta(hours=4)
+    dns_next_poll = datetime.datetime.now() + dns_poll_interval
 
     values = { 'domain' : my_domain,
                'password' : passwrd,
                'command' : api_list }
 
+    dns_ip = fetch_dns_ip(values, api_url)
+
     while (True):
+        dt = datetime.datetime.now()
+        dts = str(dt)[:16]
+
         true_ip = fetch_external_ip()
-        dns_ip = fetch_dns_ip(values, api_url)
 
         if true_ip == dns_ip:
-            print 'STILL: {0}: {1}'.format(
-                    true_ip,
-                    str(datetime.datetime.now()))
-            time.sleep(time_out)
+            if dt > dns_next_poll:
+                dns_ip = fetch_dns_ip(values, api_url)
+                dns_next_poll = dt + dns_poll_interval
+            print 'STILL: {0}: {1}'.format(true_ip, dts)
         else:
-            v2 = values
-            v2['command'] = api_replace_t.format(true_ip)
+            values['command'] = api_replace_t.format(true_ip)
             data = urllib.urlencode(values)
             req = urllib2.Request(api_url, data)
             response = urllib2.urlopen(req)
-            v2['command'] = api_list
-            print 'NOW: {0}: {1}'.format(
-                    true_ip,
-                    str(datetime.datetime.now()))
+            values['command'] = api_list
+            time.sleep(10)
+
+            dns_ip = fetch_dns_ip(values, api_url)
+            dns_next_poll = dt + dns_poll_interval
+
+            print 'NOW: {0}: {1}'.format(true_ip, dts)
+
+        time.sleep(time_out)
 
 
 if __name__ == '__main__':

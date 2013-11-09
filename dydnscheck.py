6d51b430192c834c0e7558a38e2a36293203187b
shwoop <monkey.mambo.al.ferguson@gmail.com>
2013-11-09 06:34:37 +0000

    Functionally up and running.

--- dydnscheck.py
+++ dydnscheck.py
@@ -10,15 +10,28 @@ WTFPL licensed (http://www.wtfpl.net/txt/copying/)
 
 '''
 
-import sys, re, urllib, urllib2
+import sys, time, urllib, urllib2, datetime
 
 def fetch_external_ip():
     ''' get ip address '''
     try:
-        ipecho  = unicode(urllib2.urlopen(u'http://ipecho.net/plain').read())
+        output  = unicode(urllib2.urlopen(u'http://ipecho.net/plain').read())
     except:
-        ipecho = None
-    return ipecho
+        output = None
+    return output
+
+def fetch_dns_ip(values, api_url):
+    ''' grab ip from dns record of hame subdomain. '''
+    try:
+        data = urllib.urlencode(values)
+        req = urllib2.Request(api_url, data)
+        response = urllib2.urlopen(req)
+        output = [x.split()[3] for x in
+                  response if x.startswith('hame')][0]
+    except:
+        output = None
+    return output
+
 
 def main():
     ''' pull current DNS, fetch external ip, and update when required. '''
@@ -27,30 +40,37 @@ def main():
         return -1
     passwrd = sys.argv[1]
     
-##  Config can go here because CBA
-    ip_addy = '78.105.139.36'
+    ##  Config can go here because CBA
     api_url = 'https://ctrlpanel.mythic-beasts.com/customer/primarydnsapi'
     my_domain = 'bummedinthegob.co.uk'
-    api_command = 'REPLACE hame 3600 A {0}'.format(ip_addy)
+    api_replace_t = 'REPLACE hame 3600 A {0}'
+    api_list = 'LIST'
+    time_out = 300 # seconds == 5 mins
 
-    fucks = True
-    while (fucks):
+    values = { 'domain' : my_domain,
+               'password' : passwrd,
+               'command' : api_list }
 
-        ip = fetch_external_ip()
-        print ip
+    while (True):
+        true_ip = fetch_external_ip()
+        dns_ip = fetch_dns_ip(values, api_url)
 
-        values = { 'domain' : my_domain,
-                   'password' : passwrd,
-                   'command' : api_command }
+        if true_ip == dns_ip:
+            print 'STILL: {0}: {1}'.format(
+                    true_ip,
+                    str(datetime.datetime.now()))
+            time.sleep(time_out)
+        else:
+            v2 = values
+            v2['command'] = api_replace_t.format(true_ip)
+            data = urllib.urlencode(values)
+            req = urllib2.Request(api_url, data)
+            response = urllib2.urlopen(req)
+            v2['command'] = api_list
+            print 'NOW: {0}: {1}'.format(
+                    true_ip,
+                    str(datetime.datetime.now()))
 
-        data = urllib.urlencode(values)
-        req = urllib2.Request(api_url, data)
-        response = urllib2.urlopen(req)
-        print response.read()
-
-        fucks = None
-
-    return 0
 
 if __name__ == '__main__':
     ''' when run directly fire off main entry point. '''

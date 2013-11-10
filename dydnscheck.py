e199d883bc4f50f31fa65c9830790bd87500edd5
shwoop <monkey.mambo.al.ferguson@gmail.com>
2013-11-10 03:01:02 +0000

    tidied up code w/o classes so no need for classify branch.

--- dydnscheck.py
+++ dydnscheck.py
@@ -12,22 +12,20 @@ WTFPL licensed (http://www.wtfpl.net/txt/copying/)
 
 import sys, time, urllib, urllib2, datetime
 
-def fetch_external_ip():
-    ''' get ip address '''
-    try:
-        output  = urllib2.urlopen('http://ipecho.net/plain').read()
-    except:
-        output = None
-    return output
+api_qry = {'domain' : 'bummedinthegob.co.uk',
+    'password' : None,
+    'command' : 'LIST'}
 
-def fetch_dns_ip(values, api_url):
-    ''' grab ip from dns record of hame subdomain. '''
+def url_request(values, url):
+    ''' return array from url request passing dict of parameters. '''
     try:
-        data = urllib.urlencode(values)
-        req = urllib2.Request(api_url, data)
-        response = urllib2.urlopen(req)
-        output = [x.split()[3] for x in
-                  response if x.startswith('hame')][0]
+        if values:
+            data = urllib.urlencode(values)
+            req = urllib2.Request(url, data)
+            output = urllib2.urlopen(req)
+        else:
+            output = urllib2.urlopen(url)
+        output = [x for x in output]
     except urllib2.HTTPError as e:
         print '%s : %s' % (e.code, e.reason)
         output = None
@@ -35,57 +33,87 @@ def fetch_dns_ip(values, api_url):
         print 'URLError : %s' % (e.reason)
         output = None
     except:
+        print 'Unhandled Exception'
         output = None
     return output
 
 
+def fetch_external_ip():
+    ''' get WAN ip address '''
+    ## bit of hack, should compile list and loop
+    output = url_request(None, 'http://echoip.com')
+    if not output:
+        output = url_request(None, 'http://ipecho.net/plain')
+        if not output:
+            print 'can\'t find external ip'
+    output = output[0] if output else None
+    return output
+
+
+def fetch_dns_ip(url):
+    ''' grab ip from dns record of hame subdomain. '''
+    output = url_request(api_qry, url)
+    if output:
+        output = [x.split()[3] for x in
+                  output if x.startswith('hame')][0]
+    return output
+
+def update_dns_record(command, url):
+    l_qry = api_qry
+    l_qry['command'] = command
+    output = url_request(l_qry, url)
+    return output
+
 def main():
     ''' pull current DNS, fetch external ip, and update when required. '''
+
     if len(sys.argv) != 2:
         print 'DNS api password required as argument.'
         return -1
-    passwrd = sys.argv[1]
-    
-    ##  Config can go here because CBA
-    api_url = 'https://ctrlpanel.mythic-beasts.com/customer/primarydnsapi'
-    my_domain = 'bummedinthegob.co.uk'
-    api_replace_t = 'REPLACE hame 3600 A {0}'
-    api_list = 'LIST'
-    time_out = 300 # seconds == 5 mins
-    dns_poll_interval = datetime.timedelta(hours=4)
-    dns_next_poll = datetime.datetime.now() + dns_poll_interval
 
-    values = { 'domain' : my_domain,
-               'password' : passwrd,
-               'command' : api_list }
+    ## general config
+    API_URL = 'https://ctrlpanel.mythic-beasts.com/customer/primarydnsapi'
+    API_REPLACE_T = 'REPLACE hame 3600 A %s'
+    POLL_INTERVAL = 300
+    DNS_INTERVAL = datetime.timedelta(hours=4)
 
-    dns_ip = fetch_dns_ip(values, api_url)
+    ## init
+    api_qry['password'] = sys.argv[1]
+    dns_next_poll = datetime.datetime.now() + DNS_INTERVAL
+    true_ip = None
+    dns_ip = None
+    while not true_ip:
+        true_ip = fetch_external_ip()
+        time.sleep(5)
+    while not dns_ip:
+        dns_ip = fetch_dns_ip(API_URL)
+        time.sleep(5)
 
-    while (True):
+    ## guts
+    while True:
         dt = datetime.datetime.now()
         dts = str(dt)[:16]
 
-        true_ip = fetch_external_ip()
+        new_ip = fetch_external_ip()
+        true_ip = new_ip if new_ip else true_ip
 
         if true_ip == dns_ip:
             if dt > dns_next_poll:
-                dns_ip = fetch_dns_ip(values, api_url)
-                dns_next_poll = dt + dns_poll_interval
+                new_ip = fetch_dns_ip(values, API_URl)
+                dns_ip = new_ip if new_ip else dns_ip
+                dns_next_poll = dt + POLL_INTERVAL
             print 'STILL: %s: %s' % (true_ip, dts)
+
         else:
-            values['command'] = api_replace_t % (true_ip)
-            data = urllib.urlencode(values)
-            req = urllib2.Request(api_url, data)
-            response = urllib2.urlopen(req)
-            values['command'] = api_list
+            resp = update_dns_record(API_REPLACE_T % (true_ip), API_URL)
+            print resp
             time.sleep(10)
-
-            dns_ip = fetch_dns_ip(values, api_url)
-            dns_next_poll = dt + dns_poll_interval
-
+            new_ip = fetch_dns_ip(values, API_URL)
+            dns_ip = new_ip if new_ip else dns_ip
+            dns_next_poll = dt + POLL_INTERVAL
             print 'NOW: %s: %s' % (true_ip, dts)
 
-        time.sleep(time_out)
+        time.sleep(POLL_INTERVAL)
 
 
 if __name__ == '__main__':
